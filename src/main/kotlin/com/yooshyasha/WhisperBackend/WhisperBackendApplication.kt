53a9b36a4eb46effebe93e72ae153aaccda5384c
yooshyasha <maksimmorozov962@gmail.com>
2024-10-18 23:08:02 +0500

    Initial commit

---
+++ src/main/kotlin/com/yooshyasha/WhisperBackend/WhisperBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.yooshyasha.WhisperBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhisperBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhisperBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/yooshyasha/WhisperBackend/WhisperBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yooshyasha.WhisperBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhisperBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
