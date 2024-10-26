495de14ae007732b2330daf7c3bc241101a69dab
yooshyasha <mejzikrejzi66@gmail.com>
2024-10-26 12:38:38 +0500

    clear

--- src/main/kotlin/com/yooshyasha/WhisperBackend/controller/UserController.kt
+++ src/main/kotlin/com/yooshyasha/WhisperBackend/controller/UserController.kt
@@ -1,15 +1,11 @@
 package com.yooshyasha.WhisperBackend.controller
 
 import com.yooshyasha.WhisperBackend.model.entity.User
-import com.yooshyasha.WhisperBackend.repository.UserRepository
-import com.yooshyasha.WhisperBackend.service.JwtService
 import com.yooshyasha.WhisperBackend.service.UserService
 import com.yooshyasha.WhisperBackend.service.impl.UserDetailsImpl
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,7 +15,6 @@ import java.util.*
 @RestController
 @RequestMapping("/user")
 class UserController(
-    private val jwtService: JwtService,
     private val userService: UserService,
 ) {
 
